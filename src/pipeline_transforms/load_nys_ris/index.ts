766e6b15315d488cada35fe8e959a34707b3d95a
Paul Tomchik <pauljtomchik@gmail.com>
2020-11-11 11:23:09 -0500

    Load NYS RIS. Skip problem geometries for now.

--- src/pipeline_transforms/load_nys_ris/index.ts
+++ src/pipeline_transforms/load_nys_ris/index.ts
@@ -21,58 +21,63 @@ const T_SRS = gdal.SpatialReference.fromEPSG(4326);
 
 const handleGdbEntry = (
   feature: gdal.Feature,
-): NysRoadInventorySystemGeodatabaseEntry => {
+): NysRoadInventorySystemGeodatabaseEntry | null => {
   const { fid } = feature;
+  try {
+    const properties: any = _.mapKeys(feature.fields.toObject(), (_v, k) =>
+      k.toLowerCase(),
+    );
 
-  const properties: any = _.mapKeys(feature.fields.toObject(), (_v, k) =>
-    k.toLowerCase(),
-  );
+    properties.fid = fid;
 
-  properties.fid = fid;
+    // @ts-ignore
+    const geometry: gdal.MultiLineString = feature.getGeometry();
+    geometry.transformTo(T_SRS);
 
-  // @ts-ignore
-  const geometry: gdal.MultiLineString = feature.getGeometry();
-  geometry.transformTo(T_SRS);
+    // https://github.com/naturalatlas/node-gdal/issues/263#issuecomment-520882777
+    // @ts-ignore
+    geometry.coordinateDimension = 2;
 
-  // https://github.com/naturalatlas/node-gdal/issues/263#issuecomment-520882777
-  // @ts-ignore
-  geometry.coordinateDimension = 2;
+    assert(geometry.wkbType === gdal.wkbMultiLineString);
 
-  assert(geometry.wkbType === gdal.wkbMultiLineString);
+    // @ts-ignore
+    const geojson: turf.MultiLineString = geometry.toObject();
 
-  // @ts-ignore
-  const geojson: turf.MultiLineString = geometry.toObject();
+    turf.geojsonType(geojson, 'MultiLineString', 'handleGdbEntryAsync');
 
-  turf.geojsonType(geojson, 'MultiLineString', 'handleGdbEntryAsync');
+    // Clean up the coordinates so turf.lineString or turf.multiLineString don't throw errors.
+    const geomCoords = geojson?.coordinates?.reduce(
+      (acc: turf.Position[][], positionArr) => {
+        // Remove the empty positions
+        const filteredLineStringCoords = positionArr.filter(
+          (pointCoords) => !_.isEmpty(pointCoords),
+        );
 
-  // Clean up the coordinates so turf.lineString or turf.multiLineString don't throw errors.
-  const geomCoords = geojson?.coordinates?.reduce(
-    (acc: turf.Position[][], positionArr) => {
-      // Remove the empty positions
-      const filteredLineStringCoords = positionArr.filter(
-        (pointCoords) => !_.isEmpty(pointCoords),
-      );
+        // If there are any positions, push to the multiLineString Position[][]
+        if (!_.isEmpty(filteredLineStringCoords)) {
+          acc.push(filteredLineStringCoords);
+        }
 
-      // If there are any positions, push to the multiLineString Position[][]
-      if (!_.isEmpty(filteredLineStringCoords)) {
-        acc.push(filteredLineStringCoords);
-      }
+        return acc;
+      },
+      [],
+    );
 
-      return acc;
-    },
-    [],
-  );
+    const coords = geomCoords?.length > 0 ? geojson.coordinates : null;
 
-  const coords = geomCoords?.length > 0 ? geojson.coordinates : null;
+    const shape: null | turf.Feature<turf.LineString | turf.MultiLineString> =
+      coords &&
+      // prefer LineString to MultiLineString
+      (coords?.length === 1
+        ? turf.lineString(coords[0], { fid }, { id: fid })
+        : turf.multiLineString(coords, { fid }, { id: fid }));
 
-  const shape: null | turf.Feature<turf.LineString | turf.MultiLineString> =
-    coords &&
-    // prefer LineString to MultiLineString
-    (coords?.length === 1
-      ? turf.lineString(coords[0], { fid }, { id: fid })
-      : turf.multiLineString(coords, { fid }, { id: fid }));
-
-  return { properties, shape };
+    return { properties, shape };
+  } catch (err) {
+    console.error('NYS RIS FID:', fid);
+    console.error(err);
+    return null;
+  }
 };
 
 function* makeNysRisGeodatabaseIterator(
@@ -112,7 +117,11 @@ function* makeNysRisGeodatabaseIterator(
 
   // eslint-disable-next-line
   while ((feature = features.next())) {
-    yield handleGdbEntry(feature);
+    const d = handleGdbEntry(feature);
+
+    if (d !== null) {
+      yield d;
+    }
   }
 }
 
