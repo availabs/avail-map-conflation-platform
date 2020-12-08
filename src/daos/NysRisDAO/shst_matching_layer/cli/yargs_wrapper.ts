19b2f2fdd2f763a796eb78295437017222c351ae
Paul Tomchik <pauljtomchik@gmail.com>
2020-12-08 05:52:43 -0500

    NYS RIS choose Shst matches.

--- src/daos/NysRisDAO/shst_matching_layer/cli/yargs_wrapper.ts
+++ src/daos/NysRisDAO/shst_matching_layer/cli/yargs_wrapper.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 
 import loadShstMatches from '../loaders/loadShstMatches';
-// import loadChosenShstMatches from '../loaders/loadChosenShstMatches';
+import loadChosenShstMatches from '../loaders/loadChosenShstMatches';
 
 export const shstMatchNysRis = {
   command: 'nys_ris_shst_match',
@@ -10,9 +10,9 @@ export const shstMatchNysRis = {
   handler: loadShstMatches,
 };
 
-// export const shstChooseMatchesNpmrds = {
-// command: 'npmrds_choose_shst_matches',
-// desc: 'Choose optimal SharedStreets matches for NPMRDS road Network edges.',
-// builder: {},
-// handler: loadChosenShstMatches,
-// };
+export const shstChooseMatchesNysRis = {
+  command: 'nys_ris_choose_shst_matches',
+  desc: 'Choose optimal SharedStreets matches for NYS RIS road segments.',
+  builder: {},
+  handler: loadChosenShstMatches,
+};
