3a4379a8b18ff0869d87d1903370cfa0115cb7fc
Paul Tomchik <pauljtomchik@gmail.com>
2020-12-20 11:10:46 -0500

    Added ShstFormOfWay and ShstRoadClass to the SourceMap domain.

--- src/daos/SourceMapDao/domain/types.ts
+++ src/daos/SourceMapDao/domain/types.ts
@@ -1,5 +1,13 @@
 import * as turf from '@turf/turf';
 
+import {
+  FormOfWay as SharedStreetsFormOfWay,
+  RoadClass as SharedStreetsRoadClass,
+} from 'sharedstreets-types';
+
+export { SharedStreetsFormOfWay };
+export { SharedStreetsRoadClass };
+
 export interface SharedStreetsLocationReference {
   sequence: number;
   point: [number, number];
@@ -10,16 +18,31 @@ export interface SharedStreetsLocationReference {
 
 export type SharedStreetsReferenceId = string;
 
+export interface OsmMetadataWaySection {
+  way_id: number;
+  osm_way_tags: Record<string, any>;
+  road_class: string;
+  one_way: 0 | 1;
+  roundabout: 0 | 1;
+  link: 0 | 1;
+  name: string;
+}
+
+export type OsmHighwayType = string;
+
 export interface SharedStreetsReferenceFeature
   extends turf.Feature<turf.LineString> {
   id: string;
   properties: {
     shstReferenceId: SharedStreetsReferenceId;
     geometryId: string;
-    formOfWay: string;
+    formOfWay: SharedStreetsFormOfWay;
+    roadClass: SharedStreetsRoadClass;
     fromIntersectionId: string;
     toIntersectionId: string;
     locationReferences: SharedStreetsLocationReference[];
+    osmMetadataWaySections: OsmMetadataWaySection[];
+    osmHighwayTypes: OsmHighwayType[];
   };
 }
 
