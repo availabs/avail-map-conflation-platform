959cc5148695e055f23a584d11729f898f2e07af
Paul Tomchik <pauljtomchik@gmail.com>
2021-01-21 12:09:13 -0500

    TargetMapConflationController handles ShstMatch Choosing.

--- src/services/Conflation/TargetMapConflationController/index.ts
+++ src/services/Conflation/TargetMapConflationController/index.ts
@@ -3,8 +3,43 @@
 import { TargetMapSchema } from '../../../utils/TargetMapDatabases/TargetMapDAO';
 
 import TargetMapConflationBlackboardDao from '../TargetMapConflationBlackboardDao';
+import TargetMapPathVicinity from '../TargetMapConflationHypothesesContexts/TargetMapPathVicinity';
 
-import SharedStreetsMatcherKnowledgeSource from '../TargetMapConflationKnowlegeSources/SharedStreetsMatcherKnowledgeSource';
+import SharedStreetsMatcherKnowledgeSource from '../TargetMapConflationKnowlegeSources/SharedStreets';
+
+import searchAndSort from '../TargetMapConflationKnowlegeSources/toposortTargetMapPathShstMatchedShstReferences/searchAndSort';
+import divvyToposortedTargetMapPathShstReferences from '../TargetMapConflationKnowlegeSources/divvyToposortedHypothesizedTargetMapPathShstReferences';
+
+function* createChosenMatchesIterator(
+  blkbrdDao: TargetMapConflationBlackboardDao,
+) {
+  const targetMapPathIdsIter = blkbrdDao.makeTargetMapPathIdsIterator();
+
+  for (const targetMapPathId of targetMapPathIdsIter) {
+    const vicinity = new TargetMapPathVicinity(blkbrdDao, targetMapPathId);
+
+    console.log('targetMapPath:', vicinity.targetMapPathId);
+
+    const sortedPaths = searchAndSort(vicinity);
+
+    const chosenShstMatches = divvyToposortedTargetMapPathShstReferences(
+      vicinity,
+      sortedPaths,
+    );
+
+    if (chosenShstMatches.forward) {
+      for (let i = 0; i < chosenShstMatches.forward.length; ++i) {
+        yield chosenShstMatches.forward[i];
+      }
+    }
+
+    if (chosenShstMatches.backward) {
+      for (let i = 0; i < chosenShstMatches.backward.length; ++i) {
+        yield chosenShstMatches.backward[i];
+      }
+    }
+  }
+}
 
 export default class TargetMapConflationController {
   private readonly blkbrdDao: TargetMapConflationBlackboardDao;
@@ -24,9 +59,39 @@ export default class TargetMapConflationController {
     this.blkbrdDao.vacuumDatabase();
   }
 
+  //  Pipeline's root run script SHOULD ONLY have 'conflate'
+  //    As a pattern, each Input Source or SQLite DB has a run script.
+  //      1. Load Raw
+  //      2. Load TargetMap DB
+  //      3. Conflate
+  //    To support delelopment, strive for SOLID.
+  //      Use test harnesses to run the isolated sub-steps.
   async conflate() {
     if (!this.blkbrdDao.shstMatchesAreLoaded) {
       await this.shstMatcherKnwlSrc.matchEntireTargetMap();
     }
+
+    //  Iterate over TargetMapPathVicinities
+    //    For each Vicinity
+    //      Sequence of KnowledgeSources
+    //        Ascending Levels of Abstraction
+    //          Ascending Levels of Certainty
+    //  Start with a hard-coded, simple Strategy based on proven patterns.
+    //    Architecture should support Open/Closed Principle
+    //
+    //      Software entities (classes, modules, functions, etc.)
+    //      should be open for extension, but closed for modification.
+
+    const chosenShstMatchesIter = createChosenMatchesIterator(this.blkbrdDao);
+
+    this.blkbrdDao.bulkLoadShstMatches(chosenShstMatchesIter);
+
+    // let counter = 0;
+    // for (const chosenMatches of chosenShstMatchesIter) {
+    // if (++counter === 10) {
+    // break;
+    // }
+    // console.log(JSON.stringify(chosenMatches, null, 4))
+    // }
   }
 }
