24d36376b7bfd7990ebbb3451fafe65afbe7d3ef
Paul Tomchik <pauljtomchik@gmail.com>
2021-01-21 15:47:41 -0500

    Removed deprecated code.

--- src/daos/NysRisDAO/shst_matching_layer/cli/yargs_wrapper.ts
+++ src/daos/NysRisDAO/shst_matching_layer/cli/yargs_wrapper.ts
@@ -1,18 +1,10 @@
 /* eslint-disable import/prefer-default-export */
 
 import loadShstMatches from '../loaders/loadShstMatches';
-import loadChosenShstMatches from '../loaders/loadChosenShstMatches';
 
 export const shstMatchNysRis = {
   command: 'nys_ris_shst_match',
   desc: 'Run SharedStreets matching on NYS RIS network.',
   builder: {},
   handler: loadShstMatches,
-};
-
-export const shstChooseMatchesNysRis = {
-  command: 'nys_ris_choose_shst_matches',
-  desc: 'Choose optimal SharedStreets matches for NYS RIS road segments.',
-  builder: {},
-  handler: loadChosenShstMatches,
 };
