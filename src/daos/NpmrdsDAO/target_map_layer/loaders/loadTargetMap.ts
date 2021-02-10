fbd2a26f8bb103dd21d3918fd107fcf82e5368a8
Paul Tomchik <pauljtomchik@gmail.com>
2021-02-10 07:54:45 -0500

    Removed the code that made the TargetMap databases Read-Only after loading because it introduced very difficult to resolve bugs.

--- src/daos/NpmrdsDAO/target_map_layer/loaders/loadTargetMap.ts
+++ src/daos/NpmrdsDAO/target_map_layer/loaders/loadTargetMap.ts
@@ -1,13 +1,7 @@
-import db from '../../../../services/DbService';
-
-import { NPMRDS as SCHEMA } from '../../../../constants/databaseSchemaNames';
-
 import loadMicroLevel from './loadMicroLevel';
 import loadMesoLevel from './loadMesoLevel';
 
 export default function loadTargetMap() {
-  db.makeDatabaseWritable(SCHEMA);
   loadMicroLevel();
   loadMesoLevel();
-  db.makeDatabaseReadOnly(SCHEMA);
 }
