17c60100918840da6e2210dadcae4f3a30af3c3a
Paul Tomchik <pauljtomchik@gmail.com>
2021-02-18 09:14:37 -0500

    Major cleanup of the utils/gis/snapPointSequenceToPath module.

--- src/utils/gis/snapPointSequenceToPath.ts
+++ src/utils/gis/snapPointSequenceToPath.ts
@@ -3,10 +3,28 @@
 import * as turf from '@turf/turf';
 import _ from 'lodash';
 
-const segmentizeShapeLineString = (shapeLineString) =>
-  turf.segmentReduce(
-    shapeLineString,
-    (acc, segment, _0, _1, _2, segment_idx) => {
+type LineStringSegment = turf.Feature<turf.LineString> & {
+  properties: {
+    segment_idx: number;
+    length_km: number;
+    start_dist_along: number;
+  };
+};
+
+function segmentSourceLineString(
+  sourceLineString: turf.Feature<turf.LineString>,
+): LineStringSegment[] {
+  return turf.segmentReduce(
+    sourceLineString,
+    // @ts-ignore
+    (
+      acc: LineStringSegment[],
+      segment: turf.Feature<turf.LineString>,
+      _0,
+      _1,
+      _2,
+      segment_idx,
+    ) => {
       const prevSeg = _.last(acc);
 
       const {
@@ -16,67 +34,131 @@ const segmentizeShapeLineString = (shapeLineString) =>
         } = {},
       } = prevSeg || {};
 
-      acc.push({
-        ...segment,
-        properties: {
-          segment_idx,
-          length_km: turf.length(segment),
-          start_dist_along: prevSegDistAlong + prevSegLength,
-        },
-      });
+      segment.properties = {
+        segment_idx,
+        length_km: turf.length(segment),
+        start_dist_along: prevSegDistAlong + prevSegLength,
+      };
+
+      // @ts-ignore
+      acc.push(segment);
+
       return acc;
     },
     [],
   );
+}
 
-// O(S*Ps) where S is the number of stopPoints, and Ps is the number of path segments.
-function getStopsProjectedToPathSegmentsTable(stopPoints, shapeSegments) {
+type TargetMapPointToSourceLineSegmentSnapStats = {
+  segmentNum: number;
+  pointCoords: turf.Position | null;
+  snappedCoords: turf.Position | null;
+  snappedDistAlongKm: number | null;
+  deviationKm: number;
+};
+
+type TargetMapPointToSourceLineSegmentSnapStatsTable = Array<
+  TargetMapPointToSourceLineSegmentSnapStats
+>[];
+
+// O(S*Ps) where S is the number of targetPoints, and Ps is the number of path segments.
+function getTargetMapPointToSourceLineSegmentSnapStatsTable(
+  targetPoints: turf.Feature<turf.Point>[],
+  sourceLineStringSegments: LineStringSegment[],
+): TargetMapPointToSourceLineSegmentSnapStatsTable {
   // Each stop point, mapped to each path segment
-  return stopPoints.map((stopPt) => {
-    // For each stopPt, snap it to each path segment.
-    return shapeSegments.map((segment, i) => {
-      const snapped = turf.pointOnLine(segment, stopPt);
-      const snappedCoords = snapped.geometry.coordinates;
+  return targetPoints.map((targetPoint) => {
+    // For each targetPoint, snap it to each path segment.
+    return sourceLineStringSegments.map(
+      (segment, i): TargetMapPointToSourceLineSegmentSnapStats => {
+        if (targetPoint.geometry === null) {
+          return {
+            segmentNum: i,
+            pointCoords: null,
+            snappedCoords: null,
+            snappedDistAlongKm: null,
+            deviationKm: Infinity,
+          };
+        }
 
-      const [segmentStartCoord] = turf.getCoords(segment);
-      const segmentStartPt = turf.point(segmentStartCoord);
+        try {
+          const snapped = turf.pointOnLine(segment, targetPoint);
 
-      const snappedDistTraveled =
-        turf.distance(segmentStartPt, snapped) +
-        segment.properties.start_dist_along;
+          if (snapped === null || snapped.geometry === null) {
+            return {
+              segmentNum: i,
+              pointCoords: targetPoint.geometry.coordinates,
+              snappedCoords: null,
+              snappedDistAlongKm: null,
+              deviationKm: Infinity,
+            };
+          }
 
-      const deviationKm = turf.distance(stopPt, snapped);
+          const snappedCoords = snapped.geometry.coordinates;
 
-      return {
-        segmentNum: i,
-        pointCoords: stopPt.geometry.coordinates,
-        snappedCoords,
-        snappedDistAlongKm: snappedDistTraveled,
-        deviationKm,
-      };
-    });
+          const [segmentStartCoord] = turf.getCoords(segment);
+          const segmentStartPt = turf.point(segmentStartCoord);
+
+          const snappedDistTraveled =
+            turf.distance(segmentStartPt, snapped) +
+            segment.properties.start_dist_along;
+
+          const deviationKm = turf.distance(targetPoint, snapped);
+
+          return {
+            segmentNum: i,
+            pointCoords: targetPoint.geometry.coordinates,
+            snappedCoords,
+            snappedDistAlongKm: snappedDistTraveled,
+            deviationKm,
+          };
+        } catch (err) {
+          console.error(err);
+          return {
+            segmentNum: i,
+            pointCoords: targetPoint.geometry.coordinates,
+            snappedCoords: null,
+            snappedDistAlongKm: null,
+            deviationKm: Infinity,
+          };
+        }
+      },
+    );
   });
 }
 
 // O(S W lg W) where S is the number of stops, W is the number of waypointCoords in the path.
 // Additional O(SW) space cost, as the table is replicated.
-function trySimpleMinification(theTable) {
-  const possibleOptimal = theTable.map((row) =>
-    _(row).sortBy(['deviationKm', 'snappedDistAlongKm']).first(),
+function trySimpleMinification(
+  theTable: TargetMapPointToSourceLineSegmentSnapStatsTable,
+): TargetMapPointToSourceLineSegmentSnapStats[] | null {
+  // @ts-ignore
+  const possibleOptimal: TargetMapPointToSourceLineSegmentSnapStats[] = theTable.map(
+    (row) => _(row).sortBy(['deviationKm', 'snappedDistAlongKm']).first(),
   );
 
   // If
-  function invariantCheck(projectedPointA, projectedPointB) {
+  function invariantCheck(
+    projectedPointA: TargetMapPointToSourceLineSegmentSnapStats,
+    projectedPointB: TargetMapPointToSourceLineSegmentSnapStats,
+  ) {
     return (
+      projectedPointA.snappedDistAlongKm !== null &&
+      projectedPointB.snappedDistAlongKm !== null &&
       projectedPointA.snappedDistAlongKm <= projectedPointB.snappedDistAlongKm
     );
   }
 
-  return _.tail(possibleOptimal).every((currPossOpt, i) =>
-    invariantCheck(possibleOptimal[i], currPossOpt),
-  )
-    ? possibleOptimal
-    : null;
+  for (let i = 1; i < possibleOptimal.length; ++i) {
+    const prevPossOpt = possibleOptimal[i - 1];
+    const currPossOpt = possibleOptimal[i];
+
+    if (!invariantCheck(prevPossOpt, currPossOpt)) {
+      return null;
+    }
+  }
+
+  return possibleOptimal;
 }
 
 // Finds the stops-to-path fitting with the minimum
@@ -90,49 +172,85 @@ function trySimpleMinification(theTable) {
 //
 // INTUITION: Can use spatial datastructures to speed this up? rbush? Trimming the shape?
 //            This feels completely brute force.
-function fitStopsToPathUsingLeastSquares(theTable) {
-  const [headRow] = theTable;
-  const tailRows = theTable.slice(1);
+function fitTargetPointsToSourceLineStringUsingLeastSquares(
+  theTable: TargetMapPointToSourceLineSegmentSnapStatsTable,
+) {
+  type DynamicProgrammingCell = TargetMapPointToSourceLineSegmentSnapStats & {
+    cost: number;
+    path: number[] | null;
+  };
+  type DynamicProgrammingRow = DynamicProgrammingCell[];
+  type DynamicProgrammingTable = DynamicProgrammingRow[];
 
-  // Initialize the first row.
-  headRow.forEach((cell) => {
-    cell.cost = cell.deviationKm * cell.deviationKm;
-    cell.path = [cell.segmentNum];
-  });
+  const dynProgTableFirstRow = theTable[0].map((cell) => ({
+    ...cell,
+    cost: cell.deviationKm * cell.deviationKm,
+    path: [cell.segmentNum],
+  }));
 
   // Do dynamic programing...
   //   Looking for the lowest cost path from the first row
-  tailRows.forEach((tableRow, i) => {
-    tableRow.forEach((thisCell) => {
-      // thisCell is the geospatial snapping of the stop to the shape segment.
+  // INTUITION: It seems like the no backtracking constraint can be used to
+  //            reduce the search space until it is almost linear.
+  const dynProgTable = theTable.slice(1).reduce(
+    (acc: DynamicProgrammingTable, curTableRow, prevDynProgTableRowIdx) => {
+      const prevDynProgTableRow = acc[prevDynProgTableRowIdx];
 
-      let bestFromPreviousRow = {
-        cost: Number.POSITIVE_INFINITY,
-      };
+      const curDynProgTableRow: DynamicProgrammingRow = curTableRow.map(
+        (curCell) => {
+          // curCell is the geospatial snapping of the stop to the shape segment.
 
-      theTable[i].forEach((fromCell) => {
-        // INTUITION: It seems like the no backtracking constraint can be used to
-        //            reduce the search space until it is almost linear.
-        if (
-          fromCell.snappedDistAlongKm <= thisCell.snappedDistAlongKm &&
-          fromCell.cost < bestFromPreviousRow.cost
-        ) {
-          bestFromPreviousRow = fromCell;
-        }
-      });
+          let bestFromPreviousRow: DynamicProgrammingCell | null = null;
+          let bestCostFromPreviousRow = Infinity;
 
-      // Add this best
-      thisCell.cost =
-        bestFromPreviousRow.cost + thisCell.deviationKm * thisCell.deviationKm;
+          for (let i = 0; i < prevDynProgTableRow.length; ++i) {
+            const prevRowCell: DynamicProgrammingCell = prevDynProgTableRow[i];
 
-      if (thisCell.cost < Number.POSITIVE_INFINITY) {
-        thisCell.path = bestFromPreviousRow.path.slice(); // This can be done once.
-        thisCell.path.push(thisCell.segmentNum);
-      } else {
-        thisCell.path = null;
-      }
-    });
-  });
+            // Is this prevRowCell the most optimal seen?
+            if (
+              prevRowCell.snappedDistAlongKm !== null &&
+              curCell.snappedDistAlongKm !== null &&
+              prevRowCell.snappedDistAlongKm <= curCell.snappedDistAlongKm &&
+              prevRowCell.cost < bestCostFromPreviousRow
+            ) {
+              bestCostFromPreviousRow = prevRowCell.cost;
+              bestFromPreviousRow = prevRowCell;
+            }
+          }
+
+          // If nothing was found, cost is Infinity and path is null.
+          if (
+            // Redundant, but keeps type checker happy.
+            bestFromPreviousRow === null ||
+            bestFromPreviousRow.path === null ||
+            bestCostFromPreviousRow === Infinity
+          ) {
+            return {
+              ...curCell,
+              cost: Infinity,
+              path: null,
+            };
+          }
+
+          const cost =
+            bestCostFromPreviousRow + curCell.deviationKm * curCell.deviationKm;
+
+          const path = [...bestFromPreviousRow.path, curCell.segmentNum];
+
+          return {
+            ...curCell,
+            cost,
+            path,
+          };
+        },
+      );
+
+      acc.push(curDynProgTableRow);
+
+      return acc;
+    },
+    [dynProgTableFirstRow],
+  );
 
   // Did we find a path that works satisfies the constraint???
 
@@ -142,43 +260,46 @@ function fitStopsToPathUsingLeastSquares(theTable) {
   //      * the sum of the squares of the distance between the stop's coords
   //        and the snapped point coords, otherwise.
   //   If the constraint failed for EVERY possible path, the min cost path is null.
-  const bestAssignmentOfSegments = _.minBy(_.last(theTable), 'cost').path;
+  const lastDynProgTableRow = dynProgTable[dynProgTable.length - 1];
 
-  if (bestAssignmentOfSegments) {
-    return bestAssignmentOfSegments.map((segmentNum, stopIndex) => {
-      const bestProjection = theTable[stopIndex][segmentNum];
+  const [{ path: optimalPath }] = lastDynProgTableRow.sort(
+    (a, b) => a.cost - b.cost,
+  );
 
-      return {
-        segmentNum,
-        pointCoords: bestProjection.pointCoords,
-        snappedCoords: bestProjection.snappedCoords,
-        snappedDistAlongKm: bestProjection.snappedDistAlongKm,
-        deviationKm: bestProjection.deviationKm,
-      };
-    });
+  if (optimalPath === null) {
+    return null;
   }
 
-  return null;
+  const optimalSnappings: TargetMapPointToSourceLineSegmentSnapStats[] = optimalPath.map(
+    (segmentNum, targetPointIdx) => theTable[targetPointIdx][segmentNum],
+  );
+
+  return optimalSnappings;
 }
 
-export default function fitStopsToPath(shapeLineString, stopPointsSeq) {
-  const shapeSegments = segmentizeShapeLineString(shapeLineString);
+export default function fitStopsToPath(
+  sourceLineString: turf.Feature<turf.LineString>,
+  targetPoints: turf.Feature<turf.Point>[],
+) {
+  const sourceLineStringSegments = segmentSourceLineString(sourceLineString);
 
   // first build the table
-  const theTable = getStopsProjectedToPathSegmentsTable(
-    stopPointsSeq,
-    shapeSegments,
+  const theTable = getTargetMapPointToSourceLineSegmentSnapStatsTable(
+    targetPoints,
+    sourceLineStringSegments,
   );
 
   // try the simple case
-  let stopProjections = trySimpleMinification(theTable);
+  let optimalProjections = trySimpleMinification(theTable);
 
-  if (!stopProjections) {
+  if (!optimalProjections) {
     // Simple case failed, use least squares dynamic programming.
-    stopProjections = fitStopsToPathUsingLeastSquares(theTable);
+    optimalProjections = fitTargetPointsToSourceLineStringUsingLeastSquares(
+      theTable,
+    );
   }
 
-  return Array.isArray(stopProjections) && stopProjections.length
-    ? stopProjections
+  return Array.isArray(optimalProjections) && optimalProjections.length
+    ? optimalProjections
     : null;
 }
