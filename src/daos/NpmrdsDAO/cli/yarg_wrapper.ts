369ecff29f4390fa6649e7549c70703ddc0cbd3c
Paul Tomchik <pauljtomchik@gmail.com>
2021-04-09 19:29:31 -0400

    Output NPMRDS as MBTiles.

--- src/daos/NpmrdsDAO/cli/yarg_wrapper.ts
+++ src/daos/NpmrdsDAO/cli/yarg_wrapper.ts
@@ -2,3 +2,4 @@ export * from '../raw_map_layer/cli/yargs_wrapper';
 export * from '../target_map_layer/cli/yargs_wrapper';
 export * from '../shst_matching_layer/cli/yargs_wrapper';
 export * from '../outputShapefile/yargs_wrapper';
+export * from '../outputMbtiles/yargs_wrapper';
