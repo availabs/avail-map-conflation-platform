1de8eb917a7c434783665b5e954350fa4cb9b5df
Paul Tomchik <pauljtomchik@gmail.com>
2021-06-08 16:25:18 -0400

    Added load traffic_count_station_year_directions run command.

--- src/daos/NysRisDAO/cli/yargs_wrapper.ts
+++ src/daos/NysRisDAO/cli/yargs_wrapper.ts
@@ -1,6 +1,7 @@
 export * from '../raw_map_layer/cli/yargs_wrapper';
+export * from '../traffic_count_station_year_directions/cli/yargs_wrapper';
 export * from '../target_map_layer/cli/yargs_wrapper';
 export * from '../shst_matching_layer/cli/yargs_wrapper';
-export * from '../outputShapefile/yargs_wrapper';
+// export * from '../outputShapefile/yargs_wrapper';
 export * from '../outputMbtiles/yargs_wrapper';
 export * from '../outputMbtilesForQA/yargs_wrapper';
