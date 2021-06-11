a09f2cf2b7454417a8fd8447830a828ab253c208
Paul Tomchik <pauljtomchik@gmail.com>
2021-06-11 10:54:39 -0400

    Shrinkwrap NPMRDS database.

--- src/daos/NpmrdsDAO/cli/yarg_wrapper.ts
+++ src/daos/NpmrdsDAO/cli/yarg_wrapper.ts
@@ -1,6 +1,14 @@
+import shrinkwrapDatabase from './utils/shrinkwrapDatabase';
+
 export * from '../raw_map_layer/cli/yargs_wrapper';
 export * from '../target_map_layer/cli/yargs_wrapper';
 export * from '../shst_matching_layer/cli/yargs_wrapper';
 export * from '../outputShapefile/yargs_wrapper';
 export * from '../outputMbtiles/yargs_wrapper';
 export * from '../outputMbtilesForQA/yargs_wrapper';
+
+export const shrinkwrapNpmrdsDatabase = {
+  command: 'shrinkwrap_npmrds_database',
+  desc: 'Make the NPMRDS database Read-Only',
+  handler: shrinkwrapDatabase,
+};
