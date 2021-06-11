93ce8564a319310d13c3a01e01f808f261e41e31
Paul Tomchik <pauljtomchik@gmail.com>
2021-06-11 10:56:46 -0400

    Shrinkwrap NYS RIS database.

--- src/daos/NysRisDAO/cli/yargs_wrapper.ts
+++ src/daos/NysRisDAO/cli/yargs_wrapper.ts
@@ -1,7 +1,16 @@
+import shrinkwrapDatabase from './utils/shrinkwrapDatabase';
+
 export * from '../raw_map_layer/cli/yargs_wrapper';
 export * from '../traffic_count_station_year_directions/cli/yargs_wrapper';
 export * from '../target_map_layer/cli/yargs_wrapper';
+
 export * from '../shst_matching_layer/cli/yargs_wrapper';
 // export * from '../outputShapefile/yargs_wrapper';
 export * from '../outputMbtiles/yargs_wrapper';
 export * from '../outputMbtilesForQA/yargs_wrapper';
+
+export const shrinkwrapNysRisDatabase = {
+  command: 'shrinkwrap_nys_ris_database',
+  desc: 'Make the NysRis database Read-Only',
+  handler: shrinkwrapDatabase,
+};
--- src/daos/NysRisDAO/raw_map_layer/utils/isValidNysRisVersion.ts
+++ src/daos/NysRisDAO/raw_map_layer/utils/isValidNysRisVersion.ts
@@ -1,5 +1,5 @@
 import { NysRisVersion } from '../domain/types';
 
 export default function isValidNysRisVersion(nysRisVersion: NysRisVersion) {
-  return /^[a-z0-9_-]*nys-roadway-inventory-system-\d{8}$/.test(nysRisVersion);
+  return /^[a-z0-9_-]*nys-roadway-inventory-system-v\d{8}$/.test(nysRisVersion);
 }
