4519af2bbc10e90b98147e52427b5d5253d0e1ce
Paul Tomchik <pauljtomchik@gmail.com>
2021-06-13 09:14:04 -0400

    Top level composite run commands for NYS RIS and NPMRDS.

--- src/daos/NpmrdsDAO/cli/yarg_wrapper.ts
+++ src/daos/NpmrdsDAO/cli/yarg_wrapper.ts
@@ -1,11 +1,30 @@
 import shrinkwrapDatabase from './utils/shrinkwrapDatabase';
 
+import { loadRawNpmrdsTables } from '../raw_map_layer/cli/yargs_wrapper';
+import { loadNpmrdsTargetMap } from '../target_map_layer/cli/yargs_wrapper';
+import { shstMatchNpmrds } from '../shst_matching_layer/cli/yargs_wrapper';
+
 export * from '../raw_map_layer/cli/yargs_wrapper';
 export * from '../target_map_layer/cli/yargs_wrapper';
 export * from '../shst_matching_layer/cli/yargs_wrapper';
 export * from '../outputShapefile/yargs_wrapper';
 export * from '../outputMbtiles/yargs_wrapper';
 export * from '../outputMbtilesForQA/yargs_wrapper';
+
+export const npmrdsAll = {
+  command: 'npmrds_all',
+  desc: 'Load all NPMRDS input and run conflation.',
+  builder: {
+    ...loadRawNpmrdsTables.builder,
+    ...loadNpmrdsTargetMap.builder,
+    ...shstMatchNpmrds.builder,
+  },
+  handler: async (args: any) => {
+    await loadRawNpmrdsTables.handler(args);
+    loadNpmrdsTargetMap.handler();
+    await shstMatchNpmrds.handler();
+  },
+};
 
 export const shrinkwrapNpmrdsDatabase = {
   command: 'shrinkwrap_npmrds_database',
--- src/daos/NysRisDAO/cli/yargs_wrapper.ts
+++ src/daos/NysRisDAO/cli/yargs_wrapper.ts
@@ -1,5 +1,10 @@
 import shrinkwrapDatabase from './utils/shrinkwrapDatabase';
 
+import { loadRawNysRisTables } from '../raw_map_layer/cli/yargs_wrapper';
+import { loadNysTrafficCountStations } from '../traffic_count_station_year_directions/cli/yargs_wrapper';
+import { loadNysRisTargetMap } from '../target_map_layer/cli/yargs_wrapper';
+import { shstMatchNysRis } from '../shst_matching_layer/cli/yargs_wrapper';
+
 export * from '../raw_map_layer/cli/yargs_wrapper';
 export * from '../traffic_count_station_year_directions/cli/yargs_wrapper';
 export * from '../target_map_layer/cli/yargs_wrapper';
@@ -8,6 +13,23 @@ export * from '../shst_matching_layer/cli/yargs_wrapper';
 // export * from '../outputShapefile/yargs_wrapper';
 export * from '../outputMbtiles/yargs_wrapper';
 export * from '../outputMbtilesForQA/yargs_wrapper';
+
+export const nysRisAll = {
+  command: 'nys_ris_all',
+  desc: 'Load all NYS RIS input and run conflation.',
+  builder: {
+    ...loadRawNysRisTables.builder,
+    ...loadNysTrafficCountStations.builder,
+    ...loadNysRisTargetMap.builder,
+    ...shstMatchNysRis.builder,
+  },
+  handler: async (args: any) => {
+    await loadRawNysRisTables.handler(args);
+    await loadNysTrafficCountStations.handler(args);
+    loadNysRisTargetMap.handler();
+    await shstMatchNysRis.handler();
+  },
+};
 
 export const shrinkwrapNysRisDatabase = {
   command: 'shrinkwrap_nys_ris_database',
