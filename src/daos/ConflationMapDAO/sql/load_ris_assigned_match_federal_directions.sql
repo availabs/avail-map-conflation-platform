df17974862b7cf8703e7e3fc7a2895c0243f161a
Paul Tomchik <pauljtomchik@gmail.com>
2021-06-24 10:17:16 -0400

    Optimized ConflatioMapDao ris_assigned_match_federal_directions load query.

--- src/daos/ConflationMapDAO/sql/load_ris_assigned_match_federal_directions.sql
+++ src/daos/ConflationMapDAO/sql/load_ris_assigned_match_federal_directions.sql
@@ -1,3 +1,5 @@
+-- FIXME: Replace INNER JOINs with OUTER JOINs where possible.
+
 BEGIN;
 
 DROP TABLE IF EXISTS conflation_map.ris_assigned_match_federal_directions ;
@@ -12,6 +14,91 @@ CREATE TABLE conflation_map.ris_assigned_match_federal_directions (
   PRIMARY KEY(nys_ris, is_forward)
 ) ;
 
+DROP TABLE IF EXISTS conflation_map.tmp_path_data ;
+
+CREATE TABLE conflation_map.tmp_path_data (
+  path_id                     INTEGER PRIMARY KEY,
+  target_map_path_bearing     REAL,
+  last_path_edge_idx          INTEGER NOT NULL
+) WITHOUT ROWID;
+
+INSERT INTO conflation_map.tmp_path_data (
+  path_id,
+  target_map_path_bearing,
+  last_path_edge_idx
+)
+  SELECT
+      path_id,
+      json_extract(a.properties, '$.targetMapPathBearing') AS target_map_path_bearing,
+      b.path_edge_idx AS last_path_edge_idx
+    FROM nys_ris.target_map_ppg_paths AS a
+
+      INNER JOIN nys_ris.target_map_ppg_path_last_edges AS b
+        USING (path_id)
+;
+
+ANALYZE conflation_map.tmp_path_data ;
+
+DROP TABLE IF EXISTS conflation_map.tmp_assigned_match_data ;
+
+CREATE TABLE conflation_map.tmp_assigned_match_data (
+  path_id             INTEGER NOT NULL,
+  nys_ris             TEXT NOT NULL,
+  is_forward          INTEGER NOT NULL
+) ;
+
+INSERT INTO conflation_map.tmp_assigned_match_data (
+  path_id,
+  nys_ris,
+  is_forward
+)
+  SELECT
+      a.path_id,
+      c.target_map_id AS nys_ris,
+      a.is_forward
+    FROM nys_ris_bb.target_map_edge_chosen_matches AS a
+      INNER JOIN nys_ris_bb.target_map_edge_assigned_matches AS b
+        ON (
+          ( a.edge_id = b.edge_id )
+          AND
+          ( a.is_forward = b.is_forward )
+          AND
+          ( a.shst_reference = b.shst_reference_id)
+        )
+      INNER JOIN nys_ris.target_map_ppg_edge_id_to_target_map_id AS c
+        ON ( a.edge_id = c.edge_id )
+;
+
+CREATE INDEX conflation_map.tmp_assigned_match_data_path_id_idx
+  ON tmp_assigned_match_data (path_id) ;
+
+CREATE INDEX conflation_map.tmp_assigned_match_data_ris_id_idx
+  ON tmp_assigned_match_data (nys_ris) ;
+
+ANALYZE conflation_map.tmp_assigned_match_data ;
+
+DROP TABLE IF EXISTS conflation_map.tmp_tds_data ;
+
+CREATE TABLE conflation_map.tmp_tds_data (
+  nys_ris                 TEXT PRIMARY KEY,
+  tds_rc_station          TEXT,
+  tds_federal_directions  INTEGER
+) WITHOUT ROWID ;
+
+INSERT INTO conflation_map.tmp_tds_data (
+  nys_ris,
+  tds_rc_station,
+  tds_federal_directions
+)
+  SELECT
+      target_map_id AS nys_ris,
+      json_extract(feature, '$.properties.tds_rc_station') AS tds_rc_station,
+      json_extract(feature, '$.properties.tds_federal_directions') AS tds_federal_directions
+    FROM nys_ris.raw_target_map_features
+;
+
+ANALYZE conflation_map.tmp_tds_data ;
+
 INSERT INTO conflation_map.ris_assigned_match_federal_directions (
     nys_ris,
     is_forward,
@@ -19,7 +106,7 @@ INSERT INTO conflation_map.ris_assigned_match_federal_directions (
     tds_federal_direction
   )
   SELECT
-       target_map_id AS nys_ris,
+       nys_ris,
        is_forward,
 
        tds_rc_station,
@@ -32,44 +119,25 @@ INSERT INTO conflation_map.ris_assigned_match_federal_directions (
 
     FROM (
       SELECT DISTINCT
-          e.target_map_id,
-
-          d.is_forward,
-
-          json_extract(a.properties, '$.targetMapPathBearing') AS target_map_path_bearing,
-
-          json_extract(feature, '$.properties.tds_rc_station') AS tds_rc_station,
-
-          json_extract(feature, '$.properties.tds_federal_directions') AS tds_federal_directions,
-
-          NULLIF(json_extract(feature, '$.properties.route_no'), 0) AS road_number,
+          a.nys_ris,
+          a.is_forward,
+          b.target_map_path_bearing,
+          c.tds_rc_station,
+          c.tds_federal_directions,
 
           rank() OVER (
             PARTITION BY
-              e.target_map_id,
-              d.is_forward
+              a.nys_ris,
+              a.is_forward
             ORDER BY
-              b.path_edge_idx DESC
+              b.last_path_edge_idx DESC
           ) AS path_len_rnk
 
-        FROM nys_ris.target_map_ppg_paths AS a
-          -- Used in the window function to choose the longest path.
-          INNER JOIN nys_ris.target_map_ppg_path_last_edges AS b
+        FROM conflation_map.tmp_assigned_match_data AS a
+          INNER JOIN conflation_map.tmp_path_data AS b
             USING (path_id)
-          INNER JOIN nys_ris_bb.target_map_edge_chosen_matches AS c
-            USING (path_id)
-          INNER JOIN nys_ris_bb.target_map_edge_assigned_matches AS d
-            ON (
-              ( c.edge_id = d.edge_id )
-              AND
-              ( c.is_forward = d.is_forward )
-              AND
-              ( c.shst_reference = d.shst_reference_id)
-            )
-          INNER JOIN nys_ris.target_map_ppg_edge_id_to_target_map_id AS e
-            ON ( d.edge_id = e.edge_id )
-          INNER JOIN nys_ris.raw_target_map_features AS f
-            USING (target_map_id)
+          INNER JOIN conflation_map.tmp_tds_data AS c
+            USING (nys_ris)
     )
     WHERE ( path_len_rnk = 1 ) ;
 
