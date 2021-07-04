ef023268de8a49b6880221991bdb31dffdebc2d0
Paul Tomchik <pauljtomchik@gmail.com>
2021-07-04 08:00:45 -0400

    Removed dead code.

--- src/services/Conflation/MatchesChooser/chooseTargetMapPathShstMatches/constants.ts
+++
@@ -1,4 +0,0 @@
-export const minPathLengthThld = 0.01; // 10 meters
-export const maxSegPathLengthDiffRatioThld = 0.05; // 5%
-export const maxGapDistThld = 0.002; // 2 meters
-export const maxOverlapThld = 0.002; // 2 meters
--- src/services/Conflation/MatchesChooser/computeSubGraphComponentsTraversals/removeRedundantCoords.ts
+++
@@ -1,5 +0,0 @@
-import _ from 'lodash';
-
-export default function removeRedundantCoords(coords) {
-  return coords.filter((coord, i) => !_.isEqual(coords[i - 1], coord));
-}
